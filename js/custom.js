1663b10a93f15fe2c0c6515183da07cde67f07dc
Jafrul Hasan Rasel <jafrulras@gmail.com>
2021-03-17 10:12:53 +0600

    first commit

--- js/custom.js
+++ js/custom.js
@@ -69,7 +69,21 @@ $(function(){
     }
 
     });
-    
+    //animation scroll js
+
+var html_body = $('html, body');
+$('.navbar a').on('click', function () {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+            html_body.animate({
+                scrollTop: target.offset().top - 0
+            }, 1500);
+            return false;
+        }
+    }
+});
     
 });
 
